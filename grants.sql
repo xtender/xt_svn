1c87af9e6198c1b3df488b48c9adbe89809a338f
xtender <xt.and.r@gmail.com>
2010-11-12 10:06:14 +0800

    Grants to public

--- grants.sql
+++ grants.sql
@@ -1,28 +1,25 @@
--- Getting actual permissions list:
-select * from dba_java_policy p where p.grantee=user;
+-- Getting actual permissions list to user:
+select * from dba_java_policy p where type_name='java.io.FilePermission' and p.grantee=user;
 ---------------------------------------------------------
---Granting access to executing:
-  dbms_java.grant_permission( user, 'SYS:java.io.FilePermission', '<<ALL FILES>>', 'execute' );
--- or
-  dbms_java.grant_permission( user, 'SYS:java.io.FilePermission', '/usr/bin/svn', 'execute' );
 ---------------------------------------------------------
---Granting access to read write in svn directory
-  dbms_java.grant_permission( user, 'java.io.FilePermission', '/var/svn/*', 'read,write' );
+begin
+-- Granting to executing svn
+  dbms_java.grant_permission( 'PUBLIC', 'SYS:java.io.FilePermission', '/usr/bin/svn', 'execute' );
 
---Granting access to svn subdirs  
-  dbms_java.grant_permission( user, 'java.io.FilePermission', '/var/svn/PROCEDURE/*', 'read,write' );
-  dbms_java.grant_permission( user, 'java.io.FilePermission', '/var/svn/PACKAGE/*', 'read,write' );
-  dbms_java.grant_permission( user, 'java.io.FilePermission', '/var/svn/PACKAGE BODY/*', 'read,write' );
-  dbms_java.grant_permission( user, 'java.io.FilePermission', '/var/svn/TYPE BODY/*', 'read,write' );
-  dbms_java.grant_permission( user, 'java.io.FilePermission', '/var/svn/TRIGGER/*', 'read,write' );
-  dbms_java.grant_permission( user, 'java.io.FilePermission', '/var/svn/FUNCTION/*', 'read,write' );
-  dbms_java.grant_permission( user, 'java.io.FilePermission', '/var/svn/JAVA SOURCE/*', 'read,write' );
-  dbms_java.grant_permission( user, 'java.io.FilePermission', '/var/svn/TYPE/*', 'read,write' );
--- Don't forget commit permissions changes
- commit;
+--Granting access to all users to read/write/delete in svn directory
+  dbms_java.grant_permission( 'PUBLIC', 'java.io.FilePermission', '/var/svn/-', 'read,write,delete' );
+  commit;
+end;
+
+--Or you could grant access to all with:
+--  dbms_java.grant_permission( user, 'SYS:java.io.FilePermission', '<<ALL FILES>>', 'read,write,execute' );
 ---------------------------------------------------------
--- Revoke and delete_permission:
-dbms_java.revoke_permission(user, 'java.io.FilePermission','/var/svn/JAVA SOURCE/*', 'read,write');
-dbms_java.delete_permission(PERMISSION_ID);
-commit;
+  -- Revoke and delete_permission:
+begin
+  for c in (select * from dba_java_policy p where p.grantee=user and type_name='java.io.FilePermission') loop
+      dbms_java.revoke_permission(c.grantee, 'java.io.FilePermission',c.name, c.action);
+      dbms_java.delete_permission(c.seq);
+  end loop;
+  commit;
+end;
 ---------------------------------------------------------
